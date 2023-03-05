ebb8d42f5d451ff05f9639ec751cd8805d4195aa
sershishkov <ser.shishkov@gmail.com>
2023-03-05 18:48:32 +0200

    2023.03.05

--- app.js
+++ app.js
@@ -159,3 +159,31 @@ anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
 // }
 // console.log(solution(50));
 ////////////////////////////////////////////////////////
+/////instructions start
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1, 2], [1]) == [2];
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1, 2, 2, 2, 3], [2]) == [1, 3];
+/////instructions end
+
+///mySolution
+function arrayDiff(a, b) {
+  a.forEach((el, index) => {
+    if (b.includes(el)) {
+      a.splice(index, 1);
+      console.log(a);
+    }
+  });
+  return a;
+}
+
+// console.log(arrayDiff([1, 2], [1]));
+// console.log(arrayDiff([1, 2, 2], [1]));
+// console.log(arrayDiff([1, 2, 2], [2]));
+// console.log(arrayDiff([1, 2, 2], []));
+// console.log(arrayDiff([], [1, 2]));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
+
+////////////////////////////////////////////////////////
---
+++ dist/index.js
@@ -0,0 +1,66 @@
+"use strict";
+////////////////////////////////////////////////////////
+/////instructions start
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reverseWords = void 0;
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1, 2], [1]) == [2];
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1, 2, 2, 2, 3], [2]) == [1, 3];
+/////instructions end
+///mySolution
+// function arrayDiff(a: number[], b: number[]): number[] {
+//   const newArr = a.filter((el) => {
+//     return !b.includes(el);
+//   });
+//   return newArr;
+// }
+// console.log(arrayDiff([1, 2], [1]));
+// console.log(arrayDiff([1, 2, 2], [1]));
+// console.log(arrayDiff([1, 2, 2], [2]));
+// console.log(arrayDiff([1, 2, 2], []));
+// console.log(arrayDiff([], [1, 2]));
+// console.log(arrayDiff([1, 2, 3], [1, 2]));
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+/////instructions start
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples;
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+/////instructions end
+///startSolution
+function reverseWords(str) {
+    var _a;
+    let countSpace = (_a = str.match(/ /g)) === null || _a === void 0 ? void 0 : _a.length;
+    //   console.log('countSpace:', countSpace);
+    let arrWords = str.split(' ');
+    let countWords = arrWords.length;
+    let spacesLength = countSpace / (countWords - 1);
+    let space = '';
+    for (let i = 0; i < spacesLength; i++) {
+        space += ' ';
+    }
+    let newStr = '';
+    arrWords.forEach((el, index) => {
+        newStr =
+            newStr +
+                el.split('').reverse().join('') +
+                `${index === arrWords.length - 1 ? '' : space}`;
+    });
+    return newStr;
+}
+exports.reverseWords = reverseWords;
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+console.log(reverseWords('apple'));
+console.log(reverseWords('a b c d'));
+console.log(reverseWords('double   spaced   words'));
+///endSolution
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+/////instructions start
+/////instructions end
+///startSolution
+///endSolution
+////////////////////////////////////////////////////////
